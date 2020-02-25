a9cc31139e90fa72b54e676f24a652b20ae5cbe2
ksravista <ksravista@gmail.com>
2020-02-25 02:49:52 -0500

    integrated react with spring boot

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import logo from './logo.svg';
+
 import Item from './components/Item';
 
 
@@ -9,62 +11,90 @@ import Toolbar from "@material-ui/core/Toolbar/Toolbar";
 import IconButton from "@material-ui/core/IconButton/IconButton";
 import Typography from "@material-ui/core/Typography/Typography";
 
-class App extends React.Component{
+class App extends React.Component {
 
 
-    constructor(props){
-        super(props);
-        this.state ={items:[]};
+    // constructor(props){
+    //     super(props);
+    //     this.state ={items:[]};
+    //
+    // }
+    // componentDidMount(){
+    //
+    //     this.setState({items:[{title:"Bike", description:"very big bike", price:2.3},
+    //             {title:"Bear Glasses", description:"very good looking glasses", price:2.3},
+    //             {title:"TeddyBears", description:"very big bike", price:2.3},
+    //             {title:"Bike", description:"very big bike", price:2.3},
+    //             {title:"Bike", description:"very big bike", price:2.3},
+    //             {title:"Bike", description:"very big bike", price:2.3}
+    //
+    //         ]});
+    // }
+    //
+    //
+    // render() {
+    //     return(
+    //     <Container maxWidth={"md"}r>
+    //         <AppBar position="static" style={{marginBottom:"20px" }} >
+    //             <Toolbar>
+    //                 <IconButton edge="start" color="inherit" aria-label="menu">
+    //                 </IconButton>
+    //                 <Typography variant="h6" >
+    //                     Cart
+    //                 </Typography>
+    //             </Toolbar>
+    //         </AppBar>
+    //
+    //
+    //
+    //         <Container component="main">
+    //
+    //         {this.state.items.map(item => {
+    //
+    //             return (<Item m={10} title={item.title} description={item.description} price={item.price}/>);
+    //         })}
+    //     </Container>
+    //     </Container>
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    // )
+//}
+    state = {};
 
+    componentDidMount() {
+        setInterval(this.hello, 250);
     }
-    componentDidMount(){
 
-        this.setState({items:[{title:"Bike", description:"very big bike", price:2.3},
-                {title:"Bear Glasses", description:"very good looking glasses", price:2.3},
-                {title:"TeddyBears", description:"very big bike", price:2.3},
-                {title:"Bike", description:"very big bike", price:2.3},
-                {title:"Bike", description:"very big bike", price:2.3},
-                {title:"Bike", description:"very big bike", price:2.3}
-
-            ]});
-    }
-
+    hello = () => {
+        fetch('/api/hello')
+            .then(response => response.text())
+            .then(message => {
+                this.setState({message: message});
+            });
+    };
 
     render() {
-        return(
-        <Container maxWidth={"md"}r>
-            <AppBar position="static" style={{marginBottom:"20px" }} >
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                    </IconButton>
-                    <Typography variant="h6" >
-                        Cart
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-
-
-
-            <Container component="main">
-
-            {this.state.items.map(item => {
-
-                return (<Item m={10} title={item.title} description={item.description} price={item.price}/>);
-            })}
-        </Container>
-        </Container>
-
-
-
-
-
-
-
-
-    )
-
-
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">{this.state.message}</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
     }
+
+
+
 }
 
 export default App;
---
+++ server/spring-and-react/src/test/java/no/kantega/springandreact/SpringAndReactApplicationTests.java
@@ -0,0 +1,13 @@
+package no.kantega.springandreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
