9021c3d29651007fa0e7a235140280ebac00294c
ksravista <ksravista@gmail.com>
2020-04-12 21:38:10 -0400

    initial unit tests

--- src/test/java/cart/CartApplicationTests.java
+++ src/test/java/cart/CartApplicationTests.java
@@ -1,13 +1,25 @@
 package cart;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class CartApplicationTests {
 
+	@Autowired
+	FirebaseService firebaseService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testService(){
+
+		assertTrue(false);
 	}
 
 }
