4ffeec4f417111b1794fb8a471d57d7d2421f38d
ksravista <ksravista@gmail.com>
2020-04-24 17:42:24 -0400

    new test case

--- src/test/java/cart/CartApplicationTests.java
+++ src/test/java/cart/CartApplicationTests.java
@@ -4,17 +4,35 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class CartApplicationTests {
+
 	@Autowired
 	FirebaseService firebaseService;
 	@Test
 	void contextLoads() {
 	}
+
 	@Test
 	void testService(){
+
+		try {
+			firebaseService.saveItem(new Item(656357L, "title","description", 1, 8.0));
+		} catch (ExecutionException e) {
+			assertTrue(false);
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		assertTrue(true);
+
+
 	}
 
 }
